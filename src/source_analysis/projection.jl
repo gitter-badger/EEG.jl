614fd1c407e52376fb580f14a86c04aad5d43b5e
Robert Luke <robert.g.luke@gmail.com>
2014-07-22 17:58:51 +0200

    Project to source from sensors

---
+++ src/source_analysis/projection.jl
@@ -0,0 +1,22 @@
+
+function project(s::ASSR, l::leadfield, idx::Int; verbose::Bool=true)
+
+    #
+    # This uses the source model from Van Veen et al '97
+    #
+    # For a given instance (t)
+    #
+    # measurements = headmodel * sources   =>  source = headmodel ^-1 * measurements
+    #     Nx1           Nx3    *   3x1           3x1  =    3xN        *     Nx1
+    #      x       =    H      *    m             m   =   H ^-1 = L   *      x
+    #
+
+    if verbose; println("Projecting from ASSR to source $idx"); end
+
+    # Sanity checks
+    if size(s.data,2) != size(l.L,3); error("Leadfield and data do not match"); end
+    if s.header["chanLabels"] != l.sensors; error("Sensors do not match"); end
+
+    squeeze(l.L[idx, :, :],1) * s.data'
+end
+
