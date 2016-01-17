6619129ffb3a2ffde95587aff884dc15004d74c2
Robert Luke <robert.g.luke@gmail.com>
2016-01-17 19:12:17 +0100

    Use size(a, x) instead of size(a)[x]

--- src/source_analysis/beamformers.jl
+++ src/source_analysis/beamformers.jl
@@ -1,3 +1,6 @@
+using ProgressMeter
+
+
 #######################################
 #
 # LCMV Beamformer
@@ -12,27 +15,27 @@ function beamformer_lcmv(x::Array, n::Array, H::Array; progress::Bool=false, che
     # LCMV beamformer as described in Van Veen et al '97
     #
     # Input:    x = N x M matrix     = M sample measurements on N electrodes
-    #           n = N x ? matrix     = ? sample measurement of noise on N electrodes
-    #           H = L x 3 x N matrix = forward head model.  !! Different to paper !!
+    #           n = N x M matrix     = M sample measurements of noise on N electrodes
+    #           H = L x 3 x N matrix = Forward head model.   !! Different to paper !!
 
     # This function sets everything up and checks data before passing to efficient actual calculation
 
     # Constants
-    N = size(x)[1]      # Sensors
-    M = size(x)[2]      # Samples
-    L = size(H)[1]      # Locations
+    N = size(x, 1)   # Sensors
+    M = size(x, 2)   # Samples
+    L = size(H, 1)   # Locations
 
     Logging.info("LCMV beamformer using $M samples on $N sensors for $L sources")
 
     # Some sanity checks on the input
-    if size(H)[3] != N; error("Leadfield and data dont match"); end
-    if size(H)[2] != 3; error("Leadfield dimension is incorrect"); end
+    if size(H, 3) != N; error("Leadfield and data dont match"); end
+    if size(H, 2) != 3; error("Leadfield dimension is incorrect"); end
     if N > L; error("More channels than samples. Something switched?"); end
     if any(isnan(x)); error("Their is a nan in your input x data"); end
 
     # Covariance matrices sampled at 4 times more locations than sensors
     C_x = cov(x[:, round(Int, linspace(2, M-1, 4*N))]')
-    Q   = cov(n[:, round(Int, linspace(2, size(n)[2]-1, 4*N))]')
+    Q   = cov(n[:, round(Int, linspace(2, size(n, 2)-1, 4*N))]')
 
     # Space to save results
     Variance  = Array(Float64, (L,1))         # Variance
@@ -57,10 +60,10 @@ end
 function beamformer_lcmv_actual(C_x::Array, H::Array, Q::Array; N=64, checks::Bool=false)
 
     if checks
-        if size(H)[1] != N; error("Leadfield = $(size(H)[1]) and data = $(N) dont match"); end
-        if size(H)[2] != 3; error("Leadfield dimension is incorrect"); end
-        if size(C_x)[1] != N; error("Covariance size is incorrect"); end
-        if size(C_x)[2] != N; error("Covariance size is incorrect"); end
+        if size(H, 1) != N; error("Leadfield = $(size(H, 1)) and data = $(N) dont match"); end
+        if size(H, 2) != 3; error("Leadfield dimension is incorrect"); end
+        if size(C_x, 1) != N; error("Covariance size is incorrect"); end
+        if size(C_x, 2) != N; error("Covariance size is incorrect"); end
         if size(Q) != size(C_x); error("Covariance matrices dont match"); end
     end
 
