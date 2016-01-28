7fd9278024ae1ecb7f135b7f6d478991a307e5aa
Robert Luke <robert.g.luke@gmail.com>
2016-01-28 10:06:10 +0100

    Reduce number of test voxels for beamformer
    Was exceeding TravisCI memory allocation

--- test/source_analysis/beamformers.jl
+++ test/source_analysis/beamformers.jl
@@ -12,7 +12,7 @@ x = copy(a.data')
 
 n = randn(size(x))
 
-H = randn(38*28*26, 3, 6)
+H = randn(10*9*8, 3, 6)
 
 V, N, NAI = beamformer_lcmv(x, n, H, checks=true, progress=true)
 
@@ -21,14 +21,15 @@ V, N, NAI = beamformer_lcmv(x, n, H, checks=true, progress=true)
 # Export as volume image
 #
 
-x = collect(1:27664.0)
-y = collect(1:27664.0)
-z = collect(1:27664.0)
+x = collect(1:720.0)
+y = collect(1:720.0)
+z = collect(1:720.0)
 t = [1.0]
 NAI = vec(NAI)
 
 vi = VolumeImage(NAI, "nA/cm^3", x, y, z, t, "LCMV", Dict(), "Talairach")
-NAI = reshape(NAI, (38, 28, 26, 1))
+
+NAI = reshape(NAI, (10, 9, 8, 1))
 
 write_dat(joinpath(dirname(@__FILE__), "..", "data", "tmp", "SA.dat"), 1:size(NAI,1), 1:size(NAI,2), 1:size(NAI,3), NAI, 1:size(NAI,4))
 
